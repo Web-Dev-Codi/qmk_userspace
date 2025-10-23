2612b8516b679dc7d20cbcaa6d81360d56ecc442
Web-Dev-Codi <cordiscobrian@gmail.com>
2025-10-23 16:31:46 +0200

    refactor: remove German layout and tri-layer functionality from Sofle keyboard

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -9,6 +9,9 @@ enum sofle_layers {
     _ADJUST,
 };
 
+#define RAISE MO(_RAISE)
+#define LOWER MO(_LOWER)
+
 // clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,37 +31,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTZ] = LAYOUT(
-    KC_ESC,  KC_1, KC_2, KC_3,  KC_4,    KC_5,                           KC_6 ,    KC_7, KC_8   , KC_9  , KC_0   , KC_PLUS,
-    KC_TAB,  KC_Q, KC_W, KC_E,  KC_R,    KC_T,                           KC_Z ,    KC_U, KC_I   , KC_O  , KC_P   , KC_MINS,
-    KC_LSFT, KC_A, KC_S, KC_D,  KC_F,    KC_G,                           KC_H ,    KC_J, KC_K   , KC_L  , KC_SCLN, KC_QUOT,
+    KC_ESC,  KC_1, KC_2, KC_3,  KC_4,    KC_5,                           KC_6 ,    KC_7, KC_8   , KC_9  , KC_0   ,  KC_PLUS,
+    KC_TAB,  KC_Q, KC_W, KC_E,  KC_R,    KC_T,                           KC_Z ,    KC_U, KC_I   , KC_O  , KC_P   ,  KC_MINS,
+    KC_LSFT, KC_A, KC_S, KC_D,  KC_F,    KC_G,                           KC_H ,    KC_J, KC_K   , KC_L  , KC_SCLN,  KC_QUOT,
     KC_LCTL, KC_Y, KC_X, KC_C,  KC_V,    KC_B,    KC_MUTE,          KC_NO, KC_N ,   KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LALT, LGUI(KC_LSFT), KC_LGUI, TL_LOWR, KC_SPC ,         KC_ENT, TL_UPPR, _______, KC_RGUI, KC_RCTL
+        KC_LALT, LGUI(KC_LSFT), KC_LGUI, LOWER, KC_SPC ,            KC_ENT, RAISE, _______, KC_RGUI, KC_RCTL
   ),
-
-/*
- * GERMAN (with DE_ keycodes for US OS layout)
- * Uses sendstring_german.h to produce German characters on US OS keyboard layout
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  ß   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | TAB  |   Q  |   W  |   E  |   R  |   T  |                    |   Z  |   U  |   I  |   O  |   P  |  Ü   |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |LSFT  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   Ö  |  Ä   |
- * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |LCTL  |   Y  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   -  |RSFT  |
- * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LALT |  UP  | LGUI |LOWER | /Space  /       \Enter \  |RAISE | Bspc | RGUI | RCTL |
- *            |      |      |      |      |/       /         \      \ |      |      |      |      |
- *            `----------------------------------'            '------''---------------------------'
- */
-
- [_GERMAN] = LAYOUT(
-    KC_ESC,  DE_1,    DE_2,    DE_3,    DE_4,    DE_5,                         DE_6,    DE_7,    DE_8,    DE_9,    DE_0,    DE_SS,
-    KC_TAB,  DE_Q,    DE_W,    DE_E,    DE_R,    DE_T,                         DE_Z,    DE_U,    DE_I,    DE_O,    DE_P,    DE_UDIA,
-    KC_LSFT, DE_A,    DE_S,    DE_D,    DE_F,    DE_G,                         DE_H,    DE_J,    DE_K,    DE_L,    DE_ODIA, DE_ADIA,
-    KC_LCTL, DE_Y,    DE_X,    DE_C,    DE_V,    DE_B,     KC_MUTE,       KC_NO,   DE_N,    DE_M,    DE_COMM, DE_DOT,  DE_MINS, KC_RSFT,
-                      KC_LALT, KC_UP,   KC_LGUI, TL_LOWR,   KC_SPC,      KC_ENT,  TL_UPPR,   KC_BSPC, KC_RGUI, KC_RCTL
-),
 
 
   /* LOWER                                                                Music Controls
@@ -77,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_LOWER] = LAYOUT(
-    KC_LGUI,KC_RGUI,KC_LALT,KC_RALT , KC_NO , KC_NO  ,                       KC_VOLU, KC_VOLD, KC_MRWD, KC_MFFD, KC_MSTP, KC_MPLY,
-    KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5 , KC_F6  ,                       KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
-    KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR, KC_PERC,                       KC_CIRC  , KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
-    KC_LCTL, KC_NO  , KC_NO  , KC_NO  , KC_NO , KC_NO  ,KC_MUTE,      KC_NO , KC_APP   , KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_NO  ,
-                    KC_LALT, KC_LCTL, KC_LGUI,TL_LOWR , KC_SPC ,      KC_DOWN, KC_UP ,KC_LEFT,KC_RIGHT, KC_RGHT
+    KC_LGUI,KC_RGUI , KC_LALT, KC_RALT, KC_NO  , KC_NO   ,                         KC_VOLU, KC_VOLD, KC_MRWD , KC_MFFD, KC_MSTP, KC_MPLY,
+    KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6   ,                         KC_F7  , KC_F8  , KC_F9   , KC_F10 , KC_F11 , KC_F12 ,
+    KC_GRV , KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC ,                        KC_CIRC , KC_AMPR, KC_ASTR , KC_LPRN, KC_RPRN, KC_TILD,
+    KC_LCTL, KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   , KC_MUTE,      KC_NO  , KC_APP  , KC_UNDS, KC_PLUS , KC_LCBR, KC_RCBR, KC_NO  ,
+                      KC_LALT, KC_LCTL, KC_LGUI, TL_LOWR , KC_SPC ,      KC_DOWN, KC_UP   , KC_LEFT, KC_RIGHT, KC_RGHT
   ),
 
   /* RAISE
@@ -102,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT(
     LGUI(KC_ESC)    ,LGUI(KC_SPC)    ,LAG(KC_SPC)     ,LGUI(KC_F)      ,LSG(KC_SPC)     ,LSG(KC_I)       ,                                   KC_1  ,KC_2   ,KC_3   ,_______ ,KC_UP  ,KC_BSPC ,
-    LCA(KC_DEL)     ,LSG(KC_Q)      ,LCG(KC_E)       ,LAG(KC_F)       ,LSG(KC_N)       ,LCSG(KC_SPC)    ,                                   KC_4  ,KC_5   ,KC_6   ,KC_LEFT ,_______,KC_RIGHT,
+    LCA(KC_DEL)     ,LSG(KC_Q)       ,LCG(KC_E)       ,LAG(KC_F)       ,LSG(KC_N)       ,LCSG(KC_SPC)    ,                                   KC_4  ,KC_5   ,KC_6   ,KC_LEFT ,_______,KC_RIGHT,
     LSFT(LGUI(KC_1)),LSFT(LGUI(KC_2)),LSFT(LGUI(KC_3)),LSFT(LGUI(KC_4)),LSFT(LGUI(KC_5)),LSFT(LGUI(KC_6)),                                   KC_7  ,KC_8   ,KC_9   ,_______ ,KC_DOWN,KC_DEL  ,
     KC_NO           ,KC_NO           ,KC_NO           ,KC_NO           ,KC_NO           ,KC_NO           ,KC_NO,                             KC_NO ,KC_PLUS,KC_0   , KC_PEQL,KC_LBRC,KC_RBRC, KC_TRNS,
     KC_TRNS         , KC_TRNS        ,KC_TRNS,KC_TRNS ,KC_SPC          ,                                                                     KC_ENT,KC_TRNS,KC_TRNS, KC_TRNS,KC_TRNS
@@ -163,7 +141,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_QWERTZ] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
-    [_GERMAN] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
     [_LOWER]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
     [_RAISE]  = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
     [_ADJUST] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGDN, KC_PGUP) },
